efcbc8f9ccf630420ff02c8cda034d005ada5cb9
Dylan Twaddle <dtwaddle@smu.edu>
2015-11-20 10:44:20 -0600

    finished projects 3

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	@0
+	D=A
+	@R2
+	M=D	// R2 = 0
+
+	@R0
+	D=M
+	@x
+	M=D	// x = R0
+
+	@R1
+	D=M
+	@y
+	M=D	// y = R1
+
+	
+
+(WHILE)
+	@x //condition
+	D=M
+	@FINISH
+	D;JLE	// if x <= 0 go to FINISH   
+	
+	@y //actual loop
+	D=M
+	@R2
+	M=D+M	
+	@1
+	D=A	
+	@x
+	M=M-D
+	@WHILE
+	0;JMP	
+(FINISH)	
+	@FINISH
+	0;JMP //Makes the loop infinite
