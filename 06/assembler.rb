1957dff5cbd7cf9d0b68507e3cfe8238685ffda9
Dylan Twaddle <dtwaddle@smu.edu>
2015-12-09 15:00:01 -0600

    finished cleaning up argument validation and program navigation for errors

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,10 +1,22 @@
 #! /usr/bin/env ruby
 
+def args_valid?
+	return ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length ==1
+end
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length ==1 &&
-		File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
-	asm_filename = ARGV[0]
-	puts asm_filename
-else
+def is_readable?(path)
+	File.readable?(path)
+end
+
+unless args_valid?
 	abort("Usage: assembler.rb ProgramName.asm")
 end
+
+asm_filename = ARGV[0]
+
+
+unless is_readable?(asm_filename)
+	abort("#{asm_filename} is not found or unreadable")
+end
+
+puts "The contents of #{asm_filename}"
