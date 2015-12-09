e4beb9a716317dafc4f52ac583c10fa8eea53c55
Dylan Twaddle <dtwaddle@smu.edu>
2015-12-09 15:41:05 -0600

    refactored the driver

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,5 +1,19 @@
 #! /usr/bin/env ruby
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def args_valid?
 	return ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length ==1
 end
@@ -8,23 +22,24 @@ def is_readable?(path)
 	File.readable?(path)
 end
 
+def hack_filename(asm_filename)
+	asm_basename = File.basename(asm_filename, '.asm')
+	path = File.split(asm_filename) [0]
+	"#{path}/#{asm_basename}.hack"
+end 
+
 unless args_valid?
 	abort("Usage: assembler.rb ProgramName.asm")
 end
 
 asm_filename = ARGV[0]
 
-
 unless is_readable?(asm_filename)
 	abort("#{asm_filename} is not found or unreadable")
 end
 
 File.open(asm_filename) do |asm_file|
-	asm_basename = File.basename(asm_filename, '.asm')
-	path = File.split(asm_filename) [0]
-	hack_filename = "#{path}/#{asm_basename}.hack"
-	puts hack_filename
-	File.open(hack_filename, 'w') do |hack_file|
+	File.open(hack_filename(asm_filename), 'w') do |hack_file|
 		assembler = Assembler.new(asm_file, hack_file)
 		assembler.assemble!
 	end
